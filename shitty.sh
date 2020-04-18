a8418a2cfbd8ee6e57ce8ed6b9fd34abb85e9946
patruckmrk <44691258+patruckmrk@users.noreply.github.com>
2020-04-18 17:49:09 +0700

    Create shitty.sh

---
+++ shitty.sh
@@ -0,0 +1,6 @@
+sudo apt update && apt upgrade -y
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig && mkdir build && cd build
+cmake ..
+make
